c67bc56a6e02509c22371f22364982691093c40a
v.ivanova <viktoriia.ivanova@vista.com>
2024-07-06 17:23:57 +0300

    added styles, refactoring Search Bar component

--- src/assets/components/SearchBar/SearchBar.jsx
+++ src/assets/components/SearchBar/SearchBar.jsx
@@ -1,34 +1,35 @@
 import toast, { Toaster } from 'react-hot-toast';
+import css from './SearchBar.module.css';
 
 export default function SearchBar({ onSubmit }) {
-    function handleSubmit(e) {
-        e.preventDefault();
-        const topic = e.target.elements.search.value;
-        if (topic.trim() !== '') {
-            onSubmit(topic);
-        } else {
-            toast('The search field can not be empty!',{
-                
-                style: {
-                  borderRadius: '10px',
-                  background: '#333',
-                  color: '#fff',
-                },
-              });
-        }
-      }
+  function handleSubmit(e) {
+    e.preventDefault();
+    const topic = e.target.elements.search.value.trim();
+    if (!topic) {
+      toast('The search field can not be empty!', {
+        style: {
+          borderRadius: '10px',
+          background: '#333',
+          color: '#fff',
+        },
+      });
+      return;
+    }
+    onSubmit(topic);
+  }
   return (
-    <header>
-      <form onSubmit={handleSubmit}>
+    <header className={css.header}>
+      <form className={css.form} onSubmit={handleSubmit}>
         <input
           name='search'
           type='text'
           autoComplete='off'
           autoFocus
           placeholder='Search images and photos'
+          className={css.input}
         />
         <Toaster />
-        <button type='submit'>
+        <button className={css.btnSubmit} type='submit'>
           Search
         </button>
       </form>
